f33b0b6b98f64754c8601bf488d20d8bd017c6d7
Arifalam786 <arifalam3101@gmail.com>
2023-06-25 19:20:26 +0530

    Deployed website

--- src/comp/products.js
+++ src/comp/products.js
@@ -1,4 +1,3 @@
-
 import img1 from './img/python_1.jpg'
 import img2 from './img/coursera data analysis with python_page-0001.jpg'
 import img3 from './img/coursera basic data descre_page-0001.jpg'
@@ -13,105 +12,120 @@ import img11 from './img/applied data science coursera_page-0001.jpg'
 import img12 from './img/data science with python simpli learn_page-0001.jpg'
 import img13 from './img/machine learning with python coursera_page-0001 (1).jpg'
 import img14 from './img/Coursera  deep learning_page-0001 (1).jpg'
-export const products = [
-  {
-    id: 1,
-    title: "Introduction to deep learning",
-    description: "IBM - Coursera",
-    
-    img: img14
-  },
-  {
-    id: 1,
-    title: "Machine learning with python",
-    description: "IBM - Coursera",
-    
-    img: img13
-  },
-  {
-    id: 1,
-    title: "Data science with python",
-    description: "SkillUp - Simplilearn",
-    
-    img: img12
-  },
-  {
-    id: 1,
-    title: "Applied data science capstone",
-    description: "IBM - Coursera",
-    
-    img: img11
-  },
-  {
-    id: 1,
-    title: "Data visualization with python",
-    description: "IBM - Coursera",
-    
-    img: img10
-  },
-  {
-    id: 1,
-    title: "Frontend developer intership",
-    description: "NOLOX",
-    
-    img: img9
-  },
-  {
-    id: 1,
-    title: "Programming with python",
-    description: "SpringBoard - Infosys",
-    
-    img: img8
-  },
- 
-  {
-    id: 1,
-    title: "Programming using java",
-    description: "SpringBoard - Infosys",
-    
-    img: img7
-  },
-  {
-    id: 1,
-    title: "Introduction to NoSQL database",
-    description: "IBM - Coursera",
-    
-    img: img6
-  },
-  {
-    id: 1,
-    title: "Introduction to HTML",
-    description: "Coursera project network",
-    
-    img: img5
-  },
-  {
-    id: 1,
-    title: "Python project for data science",
-    description: "IBM - Coursera",
-    
-    img: img4
-  },
-  {
-    id: 1,
-    title: "Basic data descriptors",
-    description: "RICE university - Coursera",
-    
-    img: img3
-  },
-    {
-      id: 1,
-      title: "Data analysis with python",
-      description: "IBM - Coursera",
-      
-      img: img2
+import img15 from './img/deep learning for developer infosys_page-0001.jpg'
+import img16 from './img/exploratory data analysis infosys_page-0001.jpg'
+export const products = [{
+        id: 1,
+        title: "Exploratory data analysis",
+        description: "SpringBoard - Infosys",
+
+        img: img16
     },
     {
-      id: 2,
-      title: "Python bootcamp",
-      description: "Udemy",
-     
-      img: img1
+        id: 1,
+        title: "Deep learning for developers",
+        description: "SpringBoard - Infosys",
+
+        img: img15
     },
-   
-  ];
+    {
+        id: 1,
+        title: "Introduction to deep learning",
+        description: "IBM - Coursera",
+
+        img: img14
+    },
+    {
+        id: 1,
+        title: "Machine learning with python",
+        description: "IBM - Coursera",
+
+        img: img13
+    },
+    {
+        id: 1,
+        title: "Data science with python",
+        description: "SkillUp - Simplilearn",
+
+        img: img12
+    },
+    {
+        id: 1,
+        title: "Applied data science capstone",
+        description: "IBM - Coursera",
+
+        img: img11
+    },
+    {
+        id: 1,
+        title: "Data visualization with python",
+        description: "IBM - Coursera",
+
+        img: img10
+    },
+    {
+        id: 1,
+        title: "Frontend developer intership",
+        description: "NOLOX",
+
+        img: img9
+    },
+    {
+        id: 1,
+        title: "Programming with python",
+        description: "SpringBoard - Infosys",
+
+        img: img8
+    },
+
+    {
+        id: 1,
+        title: "Programming using java",
+        description: "SpringBoard - Infosys",
+
+        img: img7
+    },
+    {
+        id: 1,
+        title: "Introduction to NoSQL database",
+        description: "IBM - Coursera",
+
+        img: img6
+    },
+    {
+        id: 1,
+        title: "Introduction to HTML",
+        description: "Coursera project network",
+
+        img: img5
+    },
+    {
+        id: 1,
+        title: "Python project for data science",
+        description: "IBM - Coursera",
+
+        img: img4
+    },
+    {
+        id: 1,
+        title: "Basic data descriptors",
+        description: "RICE university - Coursera",
+
+        img: img3
+    },
+    {
+        id: 1,
+        title: "Data analysis with python",
+        description: "IBM - Coursera",
+
+        img: img2
+    },
+    {
+        id: 2,
+        title: "Python bootcamp",
+        description: "Udemy",
+
+        img: img1
+    },
+
+];
